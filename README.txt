a8d0af656f9e6ca026bfea467e7d1755340f271a
Alexandre Vieira <AlexandreV1@bitbucket.org>
2018-11-01 22:15:51 -0300

    fixed the fix function

--- README.txt
+++ README.txt
@@ -5,4 +5,10 @@
 
 Example code: using.py (Just open and see how to use).
 
+Explanation:
+B - Beggining of a Entity;
+I - Inner part of the entity;
+L - Last part of the entity;
+U - Unitary entity;
+O - Not a entity;
 V0.1.0
--- biluo.py
+++ biluo.py
@@ -1,3 +1,18 @@
+def fix(x,y):
+    names = []
+    tags = []
+    compost = []
+    for i in range(0,len(y)):
+        if(x[i] != "O-PER"):
+            names.append(y[i])
+            if(x[i] == "L-PER" or x[i] == "U-PER"):
+                names.append(",")
+    names = " ".join(names)
+    names = names.split(",")
+    for name in names:
+        if (len(name) > 1):
+            compost.append(name)
+    return compost
 
 def startBiluo(dicts):
     import json
