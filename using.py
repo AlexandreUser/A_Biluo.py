c100c2ace9fe97bfe7a6c0595e65bfec127046e4
Alexandre Vieira <41487654+AlexandreUser@users.noreply.github.com>
2019-04-29 17:11:13 -0300

    Update using.py

--- using.py
+++ using.py
@@ -2,7 +2,6 @@ import biluo
 dicts = open("texto.txt","r");
 dicts = dicts.read();
 dicts = dicts.split(" ");
-#dicts = ["Eu","sou","Alexandre","Vieira","e","Carlos","e","meu","amigo","João","são","legais"]
 x,y = biluo.startBiluo(dicts)
 names = biluo.full_names(x,y)
 print(names)
